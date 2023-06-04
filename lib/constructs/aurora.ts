a697c7a840c5ad69f840ccbc3530940abb3c3449
non-97 <nonmonaka07@gmail.com>
2023-06-04 11:23:50 +0900

    :sparkles: Add Aurora Serverless v2 Reader

--- lib/constructs/aurora.ts
+++ lib/constructs/aurora.ts
@@ -50,27 +50,47 @@ export class Aurora extends Construct {
       }),
     });
 
+    // Monitoring Role
+    const monitoringRole = new cdk.aws_iam.Role(this, "MonitoringRole", {
+      assumedBy: new cdk.aws_iam.ServicePrincipal(
+        "monitoring.rds.amazonaws.com"
+      ),
+      managedPolicies: [
+        cdk.aws_iam.ManagedPolicy.fromAwsManagedPolicyName(
+          "service-role/AmazonRDSEnhancedMonitoringRole"
+        ),
+      ],
+    });
+
     // DB Cluster
     const clusterIdentifier = "db-cluster";
     new cdk.aws_rds.DatabaseCluster(this, "Default", {
       engine: cdk.aws_rds.DatabaseClusterEngine.auroraPostgres({
         version: cdk.aws_rds.AuroraPostgresEngineVersion.VER_15_2,
       }),
-      instanceProps: {
-        vpc: props.vpc,
-        allowMajorVersionUpgrade: false,
-        autoMinorVersionUpgrade: true,
-        deleteAutomatedBackups: true,
-        enablePerformanceInsights: true,
+      writer: cdk.aws_rds.ClusterInstance.provisioned("Instance1", {
         instanceType: cdk.aws_ec2.InstanceType.of(
-          cdk.aws_ec2.InstanceClass.BURSTABLE3,
+          cdk.aws_ec2.InstanceClass.T3,
           cdk.aws_ec2.InstanceSize.MEDIUM
         ),
+        allowMajorVersionUpgrade: false,
+        autoMinorVersionUpgrade: true,
+        enablePerformanceInsights: true,
         parameterGroup: dbParameterGroup,
         performanceInsightRetention:
           cdk.aws_rds.PerformanceInsightRetention.DEFAULT,
         publiclyAccessible: false,
-      },
+        isFromLegacyInstanceProps: true,
+        instanceIdentifier: "db-instance1",
+      }),
+      readers: [
+        cdk.aws_rds.ClusterInstance.serverlessV2("Instance2", {
+          autoMinorVersionUpgrade: false,
+          scaleWithWriter: true,
+        }),
+      ],
+      serverlessV2MaxCapacity: 1.0,
+      serverlessV2MinCapacity: 0.5,
       backup: {
         retention: cdk.Duration.days(7),
         preferredWindow: "16:00-16:30",
@@ -87,12 +107,13 @@ export class Aurora extends Construct {
       defaultDatabaseName: "testDB",
       deletionProtection: false,
       iamAuthentication: false,
-      instances: 1,
       monitoringInterval: cdk.Duration.minutes(1),
+      monitoringRole,
       parameterGroup: dbClusterParameterGroup,
       preferredMaintenanceWindow: "Sat:17:00-Sat:17:30",
       storageEncrypted: true,
       instanceIdentifierBase: "db-instance",
+      vpc: props.vpc,
       subnetGroup,
     });
   }
